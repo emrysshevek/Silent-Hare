ffde6fd6f10b4e8cc587ac7454fbb3ac5bfdf7f2
Konstantinos <kostakimonastiroglu@gmail.com>
2024-12-17 20:36:00 +0200

    dig and thump mechanics
    fixed one of the glitches

--- components/thump/thump.gd
+++ components/thump/thump.gd
@@ -13,9 +13,10 @@ func _physics_process(delta):
 	var foods_colse = get_overlapping_areas()
 	if foods_colse.size() > 0:
 		closest = foods_colse.front()
+	else:
+		closest = null
 	
 	if delay.time_left == 0 and Input.is_action_just_pressed("thump"):
-		print("iohq")
 		duration.start()
 	if duration.time_left >0:
 		thump()
@@ -25,5 +26,8 @@ func _physics_process(delta):
 	
 
 func thump():
-	arrow.look_at(closest.global_position)
-	arrow.visible = true
+	if closest and closest.is_inside_tree():
+		arrow.look_at(closest.global_position)
+		arrow.visible = true
+	else:
+		arrow.visible = false
